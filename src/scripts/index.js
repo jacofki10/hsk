653b5fe0232d2fca167f77b884375c9f4ebf7087
unknown <arnes.diego@gmail.com>
2019-08-04 19:12:01 +0900

    random button

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,7 +6,8 @@ import { hsk2 } from "./hsk2";
 //import { hsk3 } from "./hsk3";
 //import { hsk4 } from "./hsk4";
 
-//hide and show data
+
+//1.HIDE AND SHOW DATA//
 $('#kanji').change(function () {
   $('.kanji').toggleClass('hide')
 })
@@ -23,6 +24,8 @@ $('#sentence').change(function () {
   $('.sentence').toggleClass('hide')
 })
 
+
+//2.PRINT VOCABULARY IN INDEX.HTML//
 //Filter Vocabulary By Type
 function filterByType(arr, type) {
   return arr.filter(filter);
@@ -49,7 +52,7 @@ function hskLevel(array, type, hsk) {
 }
 
 function PrintType(name, type) {
-html += "<tr class='table-dark'>";
+html += "<tr class='table-dark noRandom'>";
 html += "<th colspan='5'>"+ name +"</th>";
 html += "</tr>";
 hskLevel(hsk1, type, 'hsk1');
@@ -57,7 +60,6 @@ hskLevel(hsk2, type, 'hsk2');
 //hskLevel((hsk3, 'Personal Pronoun', 'hsk3');
 //hskLevel((hsk4, 'Personal Pronoun', 'hsk4');
 }
-
 
 //print dom
 var html = '';
@@ -101,7 +103,65 @@ PrintType('Adjetive (形容词)', 'Adjetive')
 //Adverb
 PrintType('Adverb (题词)', 'Adverb')
 
-
 html += "</table>";
 document.getElementById("data_area").innerHTML = html;
 
+
+
+//3.RANDOM WORDS ON BUTTON//
+//grab the div that contains our images
+var container = $('#data_area');
+
+//grab our button
+var button = $('#random');
+
+//search through div and add all images to an array
+
+var images = $(container).find('tr');
+
+//function to shuffle our array
+//using the Fisher-Yates Shuffle.
+//See https://github.com/coolaj86/knuth-shuffle
+function shuffleArray(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+//empty our image container and
+//append our images in a new random order 
+//to the same container
+function renderImages(array){
+   $(container).empty();
+
+   $.each(array, function( index, value ) {
+      $(container).append(value);
+   });
+}
+
+//call the renderImages function when our button is pressed
+$(button).on("click", function(){
+  if(button.hasClass('on')){
+    $(".noRandom").empty();
+ //render the random images to the screen
+ var randomImages = shuffleArray(images);
+ renderImages(randomImages);
+ button.removeClass('on');
+  }
+   else{
+    document.getElementById("data_area").innerHTML = html;
+    button.addClass('on');
+   } 
+});
